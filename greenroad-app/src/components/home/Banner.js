db122f90769566fee65842821c62ed011df66cd1
unknown <royaam@code.edu.az>
2023-11-09 03:20:08 +0400

    banner section is ready

---
+++ greenroad-app/src/components/home/Banner.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import '../../assets/scss/_banner.scss'
+import banner1 from '../../assets/images/banner/banner1.jpg'
+import banner2 from '../../assets/images/banner/banner2.jpg'
+import banner3 from '../../assets/images/banner/banner3.jpg'
+
+
+
+function Banner() {
+    return (
+        <div className='container my-5'>
+            <section id='banner'>
+                <div className='row'>
+                    <div className='col-4'>
+                        <div className='wpb_wrapper d-flex'>
+                            <img className='banner-img' src={banner1} />
+                            <div class="content">
+                                <h3 class="title">Guided by local experts</h3>
+                                <div class="desc">on life and culture in their city</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='col-4'>
+                        <div className='wpb_wrapper d-flex'>
+                            <img className='banner-image' src={banner2} />
+                            <div class="content">
+                                <h3 class="title" style={{ marginLeft: '25px' }}>Top Rated in Azerbaijan</h3>
+                                <div class="desc" style={{ marginLeft: '25px' }}>1000+ reviews on Tripadvisor</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='col-4'>
+                        <div className='wpb_wrapper d-flex'>
+                            <img className='banner-img' src={banner3} />
+                            <div class="content">
+                                <h3 class="title">Top Rated in Azerbaijan</h3>
+                                <div class="desc">1000+ reviews on Tripadvisor</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
+    )
+}
+
+export default Banner
--- greenroad-app/src/pages/Home.js
+++ greenroad-app/src/pages/Home.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Banner from '../components/home/Banner'
 import Slider from '../components/home/Slider'
 import Footer from '../components/layout/Footer'
 import Navigation from '../components/layout/Navigation'
@@ -8,6 +9,7 @@ function Home() {
             <section id='header'><Navigation /> </section>
             <section id='main'>
                 <Slider />
+                <Banner />
             </section>
             <section id='footer'><Footer /></section>
         </div>
